6b29b6b89e8506ef9568177b7b002a80df5a1fb5
Adithya Venkatarao <adi_101@live.com>
2022-03-11 12:18:10 -0500

    Use indexed drawing of triangles for increased efficiency

--- DirectX11-Engine/DirectX11-Engine/Graphics.h
+++ DirectX11-Engine/DirectX11-Engine/Graphics.h
@@ -18,30 +18,44 @@ public:
 	{
 		struct Vertex
 		{
-			float x;
-			float y;
-			float z;
-			float w;
+			struct
+			{
+				float x;
+				float y;
+				float z;
+				float w;
+			} position;
+
+			struct
+			{
+				unsigned char r;
+				unsigned char g;
+				unsigned char b;
+				unsigned char a;
+			} color;
 		};
 
-		// create vertex buffer (1 2d triangle at center of screen)
+		// create vertex buffer
 		const Vertex vertices[] =
 		{
-			{ 0.0f,0.5f, 0.0f, 1.0f },
-			{ 0.5f,-0.5f, 0.0f, 1.0f },
-			{ -0.5f, -0.5f, 0.0f, 1.0f },
+			{ 0.0f, 0.5f, 0.0f, 1.0f,	255, 0, 0, 0 },
+			{ 0.5f,-0.5f, 0.0f, 1.0f,	0, 255, 0, 0 },
+			{ -0.5f, -0.5f, 0.0f, 1.0f,	0, 0, 255, 0 },
+			{ -0.3f, 0.3f, 0.0f, 1.0f,	0, 255, 0, 0 },
+			{ 0.3f, 0.3f, 0.0f, 1.0f,	0, 0, 255, 0 },
+			{ 0.0f, -0.8f, 0.0f, 1.0f,	255, 0, 0, 0 },
 		};
 		Microsoft::WRL::ComPtr<ID3D11Buffer> pVertexBuffer;
-		D3D11_BUFFER_DESC vertexShaderBufferDesc = {};
-		vertexShaderBufferDesc.BindFlags = D3D11_BIND_VERTEX_BUFFER;
-		vertexShaderBufferDesc.Usage = D3D11_USAGE_DEFAULT;
-		vertexShaderBufferDesc.CPUAccessFlags = 0u;
-		vertexShaderBufferDesc.MiscFlags = 0u;
-		vertexShaderBufferDesc.ByteWidth = sizeof(vertices);
-		vertexShaderBufferDesc.StructureByteStride = sizeof(Vertex);
-		D3D11_SUBRESOURCE_DATA subresourceData = {};
-		subresourceData.pSysMem = vertices;
-		CHECK_HRESULT(pDevice->CreateBuffer(&vertexShaderBufferDesc, &subresourceData, &pVertexBuffer));
+		D3D11_BUFFER_DESC vertexBufferDesc = {};
+		vertexBufferDesc.BindFlags = D3D11_BIND_VERTEX_BUFFER;
+		vertexBufferDesc.Usage = D3D11_USAGE_DEFAULT;
+		vertexBufferDesc.CPUAccessFlags = 0u;
+		vertexBufferDesc.MiscFlags = 0u;
+		vertexBufferDesc.ByteWidth = sizeof(vertices);
+		vertexBufferDesc.StructureByteStride = sizeof(Vertex);
+		D3D11_SUBRESOURCE_DATA vertexBufferSubresourceData = {};
+		vertexBufferSubresourceData.pSysMem = vertices;
+		CHECK_HRESULT(pDevice->CreateBuffer(&vertexBufferDesc, &vertexBufferSubresourceData, &pVertexBuffer));
 
 		// set primative topology
 		pDeviceContext->IASetPrimitiveTopology(D3D10_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
@@ -50,6 +64,29 @@ public:
 		const UINT stride = sizeof(Vertex);
 		const UINT offset = 0u;
 		pDeviceContext->IASetVertexBuffers(0u, 1u, pVertexBuffer.GetAddressOf(), &stride, &offset);
+
+		// create index buffer
+		const unsigned short indices[] =
+		{
+			0, 1, 2,
+			0, 2, 3,
+			0, 4, 1,
+			2, 1, 5
+		};
+		Microsoft::WRL::ComPtr<ID3D11Buffer> pIndexBuffer;
+		D3D11_BUFFER_DESC indexBufferDesc = {};
+		indexBufferDesc.BindFlags = D3D11_BIND_INDEX_BUFFER;
+		indexBufferDesc.Usage = D3D11_USAGE_DEFAULT;
+		indexBufferDesc.CPUAccessFlags = 0u;
+		indexBufferDesc.MiscFlags = 0u;
+		indexBufferDesc.ByteWidth = sizeof(indices);
+		indexBufferDesc.StructureByteStride = sizeof(unsigned short);
+		D3D11_SUBRESOURCE_DATA indexBufferSubresourceData = {};
+		indexBufferSubresourceData.pSysMem = indices;
+		CHECK_HRESULT(pDevice->CreateBuffer(&indexBufferDesc, &indexBufferSubresourceData, &pIndexBuffer));
+
+		// bind index buffer
+		pDeviceContext->IASetIndexBuffer(pIndexBuffer.Get(), DXGI_FORMAT_R16_UINT, 0u);
 
 		Microsoft::WRL::ComPtr<ID3DBlob> pBlob;
 		// create vertex shader
@@ -64,7 +101,8 @@ public:
 		Microsoft::WRL::ComPtr<ID3D11InputLayout> pInputLayout;
 		const D3D11_INPUT_ELEMENT_DESC inputElementDescriptor[] =
 		{
-			{ "Position", 0, DXGI_FORMAT_R32G32B32A32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0 }
+			{ "Position", 0, DXGI_FORMAT_R32G32B32A32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0 },
+			{ "Color", 0, DXGI_FORMAT_R8G8B8A8_UNORM, 0, D3D11_APPEND_ALIGNED_ELEMENT, D3D11_INPUT_PER_VERTEX_DATA, 0 }, // D3D11_APPEND_ALIGNED_ELEMENT should be 16 since the position has 4 * 4-byte values
 		};
 
 		CHECK_HRESULT(pDevice->CreateInputLayout(inputElementDescriptor, static_cast<UINT>(std::size(inputElementDescriptor)), pBlob->GetBufferPointer(), pBlob->GetBufferSize(), &pInputLayout));
@@ -83,15 +121,15 @@ public:
 
 		// configure viewport
 		D3D11_VIEWPORT viewport;
-		viewport.Width = 1280;
-		viewport.Height = 720;
-		viewport.MinDepth = 0;
-		viewport.MaxDepth = 1;
-		viewport.TopLeftX = 0;
-		viewport.TopLeftY = 0;
+		viewport.Width = 1280.0f;
+		viewport.Height = 720.0f;
+		viewport.MinDepth = 0.0f;
+		viewport.MaxDepth = 1.0f;
+		viewport.TopLeftX = 0.0f;
+		viewport.TopLeftY = 0.0f;
 		pDeviceContext->RSSetViewports(1u, &viewport);
 
-		pDeviceContext->Draw(static_cast<UINT>(std::size(vertices)), 0u);
+		pDeviceContext->DrawIndexed(static_cast<UINT>(std::size(indices)), 0u, 0u);
 	}
 	void EndFrame();
 private:
--- DirectX11-Engine/DirectX11-Engine/VertexShader.hlsl
+++ DirectX11-Engine/DirectX11-Engine/VertexShader.hlsl
@@ -1,4 +1,13 @@
-float4 main( float4 pos : POSITION ) : SV_POSITION
+struct VSOut
 {
-	return pos;
+	float4 color : Color;
+	float4 position : SV_Position;
+};
+
+VSOut main(float4 pos : POSITION, float4 color : Color)
+{
+	VSOut vso;
+	vso.position = pos;
+	vso.color = color;
+	return vso;
 }
